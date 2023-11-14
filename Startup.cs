964d71ad012eecdd210d4d1393a9e80b77d4ffa1
johnny-camby <johnny.kikambi@gmail.com>
2023-11-14 11:20:14 +0100

    Startup class

---
+++ Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(GithubActionsStudy.Startup))]
+
+namespace GithubActionsStudy
+{
+    class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
+
